abb518ddf24e4b5fbb39fa117d492f2811309d36
HartmannSa <68069890+HartmannSa@users.noreply.github.com>
2020-12-02 17:13:47 +0100

    Detect DVDs with ORB works

--- mir_vision/nodes/FeatureDetection.py
+++ mir_vision/nodes/FeatureDetection.py
@@ -9,59 +9,67 @@ import roslib
 import rospy
 from sensor_msgs.msg import CompressedImage
 
-VERBOSE=False
+VERBOSE=True
 
-class image_feature:
-    def __init__(self, path):
+class object_detection:
+    features = {'AKAZE', 'KAZE', 'ORB', 'BRISK', 'SIFT', 'SURF'}
+
+    def __init__(self, path, detector='ORB'):
         '''Initialize ros publisher, ros subscriber'''
         self.image_pub = rospy.Publisher("/output/image_raw/compressed",
             CompressedImage, queue_size=500)
             
-        self.path = path
-        self.CreateMatches()
+        self.path = path            # Path to class_images/ Trainimages
+        self.detector = detector    # Which Feature Detector is used
+        self.CreateMatches()        # Detect Features in Trainimages
         self.subscriber = rospy.Subscriber("/camera/color/image_raw/compressed",
             CompressedImage, self.callback,  queue_size = 1)
         if VERBOSE :
             print "subscribed to /camera/color/image_raw/compressed"
 
     def findDes(self, images):
+        if (self.detector=='ORB'):        
+            self.det = cv2.ORB_create(nfeatures=1000)
+        elif (self.detector=='SIFT'):
+            pass
+        else:
+            print('Detector ' + self.detector + ' is not implemented yet')
+            pass             
         desList=[]
         kpList=[]
         for img in images:
-            kp,des = self.orb.detectAndCompute(img,None)
+            kp,des = self.det.detectAndCompute(img,None)
             desList.append(des)  
-            kpList.append(kp)
-            imgKp = cv2.drawKeypoints(img,kp,None)            
+            kpList.append(kp)            
         return kpList, desList
      
     def CreateMatches(self):
-        '''Creates Matches'''        
-        self.orb = cv2.ORB_create(nfeatures=1000)
-        #### Import Images
-        images = []
+        '''To creates Matches features in Trainimages needs to be detected'''        
+        # Generate classNames from Trainimages
+        self.images_train = []
         self.classNames= []
         myList = os.listdir(self.path)        
         for cl in myList:
             imgCur = cv2.imread(self.path + '/' + cl, 0) 
-            images.append(imgCur)
+            self.images_train.append(imgCur)
             self.classNames.append(os.path.splitext(cl)[0])
-        
-        print('Images')
-        print(images)
-        self.desList = self.findDes(images)
         if VERBOSE :
-            print('Total Classes Detected', len(myList))
-            print(self.classNames)                   
-            print(len(self.desList))
+            print('Total Classes', len(myList))
+            print(self.classNames) 
+        # Generate Descriptors from Trainimages
+        self.kpList_Train, self.desList_Train = self.findDes(self.images_train)
+        # print(len(self.kpList_Train[0]))
+        if VERBOSE :              
+            print('Number of Descriptors', len(self.desList_Train))
 
-    def findID(self, img, desList,thres=15):
-        kp2,des2 = self.orb.detectAndCompute(img,None)
-        bf = cv2.BFMatcher()
+    def findClassID(self, img,thres=20):
+        kp2,des2 = self.det.detectAndCompute(img,None)
+        matcher = cv2.BFMatcher()
         matchList=[]
         finalVal = -1
         try:
-            for des in desList:
-                matches = bf.knnMatch(des, des2, k=2)
+            for des in self.desList_Train:
+                matches = matcher.knnMatch(des, des2, k=2)
                 good = []
                 for m, n in matches:
                     if m.distance < 0.75 * n.distance:
@@ -69,15 +77,35 @@ class image_feature:
                 matchList.append(len(good))
         except:
             pass
-        # print(matchList)
+        if VERBOSE:
+            print('Good Matches per Class')
+            print(matchList)
+        # Give back Id (finalVal) of matching class and show matches in image    
         if len(matchList)!=0:
             if max(matchList) > thres:
                 finalVal = matchList.index(max(matchList))
+                if (VERBOSE and finalVal != -1):
+                    #Matchpoints erneut generieren
+                    matches = matcher.knnMatch(self.desList_Train[finalVal], des2, k=2)
+                    good = []
+                    for m, n in matches:
+                        if m.distance < 0.75 * n.distance:
+                            good.append([m])
+                    # Show the image with matches
+                    img_matches = cv2.drawMatchesKnn(self.images_train[finalVal], self.kpList_Train[finalVal],img, kp2, matches[:20],None, flags=2)
+                    img_matches = cv2.resize(img_matches, (1000,650)) 
+                    cv2.imshow("Matches", img_matches) 
+                    cv2.waitKey(1)                    
+            elif (VERBOSE):
+                try:
+                    cv2.destroyWindow("Matches") 
+                except:
+                    pass
         return finalVal
 
     def callback(self, ros_data):
         '''Callback function of subscribed topic. 
-        Here images get converted and features detected'''
+        Here camera-images get converted, features detected and objects classified'''
         if VERBOSE :
             print 'received image of type: "%s"' % ros_data.format
 
@@ -86,11 +114,11 @@ class image_feature:
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
         image_g = cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY)
         
-        id = self.findID(image_g,self.desList)
+        id = self.findClassID(image_g)
         if id != -1:
-            cv2.putText(image_np,self.classNames[id],(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),2)
+            cv2.putText(image_np,self.classNames[id],(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
  
-        cv2.imshow('img2',image_np)
+        cv2.imshow('img_np',image_np)
         # img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
         cv2.waitKey(1)       
         
@@ -106,8 +134,8 @@ class image_feature:
 
 def main(args):
     '''Initializes and cleanup ros node'''
-    rospy.init_node('image_feature', anonymous=True)
-    ic = image_feature('/home/rosmatch/catkin_ws_hartmann/src/Mir200_Sim/mir_vision/classes')
+    rospy.init_node('object_detection', anonymous=True)
+    od = object_detection('/home/rosmatch/catkin_ws_hartmann/src/Mir200_Sim/mir_vision/classes')
     try:
         rospy.spin()
     except KeyboardInterrupt:
